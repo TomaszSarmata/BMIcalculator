3902f148b407cef53200c32ae7ab0f82a7510fde
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-01-12 13:33:08 +0000

    back to the original varsion to be able to pull

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ form.addEventListener('submit', function(e) {
     const guide = document.querySelector('#guide')
     guide.classList.remove('invisible')
 
-    if ((height === '') || (height === ' ') || (height <= 0) || (isNaN(height))) {
+    if ((height === '') || (height <= 0) || (isNaN(height))) {
         result.innerHTML = 'Please provide a valid height e.g. 1.8'
-    } else if ((weight === '') || (weight === ' ') || (weight <=  0) || (isNaN(weight))){
+    } else if ((weight === '') || (weight <=  0) || (isNaN(weight))){
         result.innerHTML = 'Please provide a valid weight e.g. 86'
     } else {
         const BMI = Math.round(weight / (height ** 2))
